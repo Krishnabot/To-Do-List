e03420db21ef388bc5c2b96ad5a5e0ca59c23feb
Krishna <libraryismyparadise@gmail.com>
2022-07-21 21:42:29 +0545

    Add test for adding and removing elements in taskList

---
+++ __test__/addremove.test.js
@@ -0,0 +1,29 @@
+import {
+  addTask,
+  deleteTask,
+  getLocalStorage,
+  setLocalStorage,
+  addToDoTask,
+  deleteToDoTask,
+  editToDoTask,
+} from '../src/addremove.js';
+
+describe('Add, Remove Task', () => {
+  document.body.innerHTML = '<div class="to-do-list"></div>';
+});
+
+const task = { description: 'text', completed: false, index: 1 };
+
+test(' Test to add a task in the TaskList', () => {
+  addToDoTask(task);
+  const TaskList = document.querySelectorAll('.to-do-list div');
+  expect(TaskList).toHaveLength(1);
+  expect(JSON.parse(localStorage.getItem('Task'))).toHaveLength(1);
+});
+
+test('Test to remove task from TaskList', () => {
+  const RemoveTask = document.querySelectorAll('.remove-task');
+  deleteTask(RemoveTask[0]);
+  const TaskList = document.querySelectorAll('.to-do-list div');
+  expect(TaskList).toHaveLength(0);
+});
