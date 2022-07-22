8ac2f0c46554c8774d58b852f128d4663053bbd5
Krishna <libraryismyparadise@gmail.com>
2022-07-22 21:54:33 +0545

    delete unnecessar import in interactive test

--- __test__/interactive_list.test.js
+++ __test__/interactive_list.test.js
@@ -1,31 +1,27 @@
-import { TestEnvironment } from "jest-environment-jsdom";
 import {
-  deleteTask,
-  getLocalStorage,
   addToDoTask,
-  deleteToDoTask,
-} from "../src/addremove.js";
-import { editCheckBox } from "../src/interactive_list.js";
+} from '../src/addremove.js';
+import { editCheckBox, clearCompletedTask } from '../src/interactive_list.js';
 
-describe(" Editcheckbok Clear completed Test", () => {
+describe(' Editcheckbok Clear completed Test', () => {
   document.body.innerHTML = '<div class="to-do-list"></div>';
-  const task = { description: "text", completed: false, index: 1 };
+  const task = { description: 'text', completed: false, index: 1 };
   addToDoTask(task);
   const tasks = [
-    { description: "Task One", completed: false, index: 1 },
-    { description: "Task Two", completed: false, index: 1 },
-    { description: "Task Three", completed: false, index: 1 },
+    { description: 'Task One', completed: false, index: 1 },
+    { description: 'Task Two', completed: false, index: 1 },
+    { description: 'Task Three', completed: false, index: 1 },
   ];
-  localStorage.setItem("Task", JSON.stringify(tasks));
+  localStorage.setItem('Task', JSON.stringify(tasks));
 
-  test("Test The editCheckbok", () => {
+  test('Test The editCheckbok', () => {
     const ID = 1;
     const event = true;
     editCheckBox(ID, event);
-    expect(JSON.parse(localStorage.getItem("Task"))[1].completed).toBeTruthy();
+    expect(JSON.parse(localStorage.getItem('Task'))[1].completed).toBeTruthy();
 
     const anotherEvent = false;
     editCheckBox(ID, anotherEvent);
-    expect(JSON.parse(localStorage.getItem("Task"))[1].completed).toBeFalsy();
+    expect(JSON.parse(localStorage.getItem('Task'))[1].completed).toBeFalsy();
   });
 });
