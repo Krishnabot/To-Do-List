8ecf6534c78b607b01aa004cf4b421396ca09645
Krishna <libraryismyparadise@gmail.com>
2022-07-23 02:11:08 +0545

    Add Test for Edit TAsk

--- __test__/interactive_list.test.js
+++ __test__/interactive_list.test.js
@@ -1,48 +1,63 @@
-import {
-  addToDoTask,
-} from '../src/addremove.js';
+import { addToDoTask, editToDoTask } from '../src/addremove.js';
 import { editCheckBox, clearCompletedTask } from '../src/interactive_list.js';
 
-describe(' Editcheckbok Clear completed Test', () => {
+describe('Test Interactive List functions test', () => {
   document.body.innerHTML = '<div class="to-do-list"></div>';
   const task = { description: 'text', completed: false, index: 1 };
   addToDoTask(task);
   const tasks = [
     { description: 'Task One', completed: false, index: 1 },
-    { description: 'Task Two', completed: false, index: 1 },
-    { description: 'Task Three', completed: false, index: 1 },
+    { description: 'Task Two', completed: false, index: 2 },
+    { description: 'Task Three', completed: false, index: 3 },
   ];
   localStorage.setItem('Task', JSON.stringify(tasks));
 
-  test('Test The editCheckbok', () => {
-    const ID = 1;
+  test('check the edit check box', () => {
     const event = true;
-    editCheckBox(ID, event);
-    expect(JSON.parse(localStorage.getItem('Task'))[1].completed).toBeTruthy();
-
-    const anotherEvent = false;
-    editCheckBox(ID, anotherEvent);
-    expect(JSON.parse(localStorage.getItem('Task'))[1].completed).toBeFalsy();
+    const id = 1;
+    editCheckBox(id, event);
+    expect(JSON.parse(localStorage.getItem('Task'))[0].completed).toBeFalsy();
+    const secondEvent = false;
+    editCheckBox(id, secondEvent);
+    expect(JSON.parse(localStorage.getItem('Task'))[0].completed).toBeFalsy();
   });
-test('Test clear complerted function', () => {
-const tasks = [
-{ description: 'Task one', completed: true, index: 1 },
-{ description: 'Task two', completed: false, index: 2 },
-{ description: 'Task three', completed: false, index: 3 },
-];
 
-localStorage.setItem('Task', JSON.stringify(tasks));
-clearCompletedTask();
-expect(JSON.parse(localStorage.getItem('Task'))).toHaveLength(2);
+  test('Test clear complerted function', () => {
+    const tasks = [
+      { description: 'Task one', completed: true, index: 1 },
+      { description: 'Task two', completed: false, index: 2 },
+      { description: 'Task three', completed: false, index: 3 },
+    ];
 
-const anotherTasks = [
-{ description: 'text', completed: false, index: 1 },
-{ description: 'two', completed: false, index: 2 },
-{ description: 'three', completed: false, index: 3 },
-];
+    localStorage.setItem('Task', JSON.stringify(tasks));
+    clearCompletedTask();
+    expect(JSON.parse(localStorage.getItem('Task'))).toHaveLength(2);
 
-localStorage.setItem('Task', JSON.stringify(anotherTasks));
-clearCompletedTask();
-expect(JSON.parse(localStorage.getItem('Task'))).toHaveLength(3);
-}); 
+    const anotherTasks = [
+      { description: 'text', completed: false, index: 1 },
+      { description: 'two', completed: false, index: 2 },
+      { description: 'three', completed: false, index: 3 },
+    ];
+
+    localStorage.setItem('Task', JSON.stringify(anotherTasks));
+    clearCompletedTask();
+    expect(JSON.parse(localStorage.getItem('Task'))).toHaveLength(3);
+  });
+
+  test('check the function for editing the task description', () => {
+    const task = [{ description: 'text', completed: true, index: 1 }];
+    localStorage.setItem('List', JSON.stringify(task));
+    const value = 'testing testing';
+    const id = 1;
+    editToDoTask(id, value);
+    expect(JSON.parse(localStorage.getItem('Task'))[0].description).toBe(
+      'testing testing',
+    );
+
+    const anotherValue = 'Hello There';
+    editToDoTask(id, anotherValue);
+    expect(JSON.parse(localStorage.getItem('Task'))[0].description).toBe(
+      'Hello There',
+    );
+  });
 });
