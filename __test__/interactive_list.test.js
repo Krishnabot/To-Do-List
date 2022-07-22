6d8cd68cde59f6a1ca9a8ccf24211270af387c85
Krishna <libraryismyparadise@gmail.com>
2022-07-22 21:53:06 +0545

    Add testting for editcheckbox

---
+++ __test__/interactive_list.test.js
@@ -0,0 +1,31 @@
+import { TestEnvironment } from "jest-environment-jsdom";
+import {
+  deleteTask,
+  getLocalStorage,
+  addToDoTask,
+  deleteToDoTask,
+} from "../src/addremove.js";
+import { editCheckBox } from "../src/interactive_list.js";
+
+describe(" Editcheckbok Clear completed Test", () => {
+  document.body.innerHTML = '<div class="to-do-list"></div>';
+  const task = { description: "text", completed: false, index: 1 };
+  addToDoTask(task);
+  const tasks = [
+    { description: "Task One", completed: false, index: 1 },
+    { description: "Task Two", completed: false, index: 1 },
+    { description: "Task Three", completed: false, index: 1 },
+  ];
+  localStorage.setItem("Task", JSON.stringify(tasks));
+
+  test("Test The editCheckbok", () => {
+    const ID = 1;
+    const event = true;
+    editCheckBox(ID, event);
+    expect(JSON.parse(localStorage.getItem("Task"))[1].completed).toBeTruthy();
+
+    const anotherEvent = false;
+    editCheckBox(ID, anotherEvent);
+    expect(JSON.parse(localStorage.getItem("Task"))[1].completed).toBeFalsy();
+  });
+});
