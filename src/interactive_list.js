d8340c084cc19eb6a9ab9d0c9921b91dc12e7d07
Krishna <libraryismyparadise@gmail.com>
2023-05-20 23:05:21 +0545

    Refactor interactive_list.js

--- src/interactive_list.js
+++ src/interactive_list.js
@@ -1,27 +1,32 @@
+// Refactored code
+
 import { setLocalStorage, addTask } from './addremove.js';
 
 let Task = JSON.parse(localStorage.getItem('Task')) || [];
 
+// Update the 'completed' property of a task
 const editCheckBox = (ID, event) => {
-  Task = JSON.parse(localStorage.getItem('Task')) || [];
-  Task.forEach((element) => {
-    if (element.index === ID) {
-      element.completed = event;
+  Task.forEach((task) => {
+    if (task.index === ID) {
+      task.completed = event;
     }
-    setLocalStorage(Task);
   });
+
+  setLocalStorage(Task);
 };
 
+// Clear completed tasks
 const clearCompletedTask = () => {
-  Task = JSON.parse(localStorage.getItem('Task'));
-  Task = Task.filter((element) => element.completed === false);
+  Task = Task.filter((task) => !task.completed);
 
-  Task.forEach((element, index) => {
-    element.index = index + 1;
+  Task.forEach((task, index) => {
+    task.index = index + 1;
   });
+
   setLocalStorage(Task);
 
-  document.querySelector('.to-do-list').innerHTML = '';
+  const taskList = document.querySelector('.to-do-list');
+  taskList.innerHTML = '';
   addTask(Task);
 };
 
