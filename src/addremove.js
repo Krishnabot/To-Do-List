3819d101847af8521cca20b3281085615339c862
Krishna <libraryismyparadise@gmail.com>
2023-05-20 23:04:07 +0545

    Refactor addremove.js

--- src/addremove.js
+++ src/addremove.js
@@ -1,53 +1,65 @@
+// Refactored code
+
+// Retrieve the Task from localStorage or create an empty array if it doesn't exist
 const Task = JSON.parse(localStorage.getItem('Task')) || [];
 
+// Create an HTML element for a task
 const createElement = (element) => {
-  const InsertedDiv = document.createElement('div');
-  const TaskList = document.querySelector('.to-do-list');
-  const InsertedCheckBox = document.createElement('input');
+  const insertedDiv = document.createElement('div');
+  const taskList = document.querySelector('.to-do-list');
+  const insertedCheckBox = document.createElement('input');
   const toDoTask = document.createElement('input');
   const deleteButton = document.createElement('button');
 
   deleteButton.classList.add('remove-task');
   deleteButton.innerHTML = 'Remove';
 
-  InsertedDiv.classList.add('dynamic-Elements');
+  insertedDiv.classList.add('dynamic-Elements');
 
-  InsertedCheckBox.type = 'checkbox';
-  InsertedCheckBox.classList.add('insertedcheckbox');
-  InsertedCheckBox.checked = element.complete;
+  insertedCheckBox.type = 'checkbox';
+  insertedCheckBox.classList.add('insertedcheckbox');
+  insertedCheckBox.checked = element.completed;
 
   toDoTask.value = element.description;
   toDoTask.classList.add('task-layout');
 
-  InsertedDiv.setAttribute('index_id', element.index);
-  InsertedDiv.append(InsertedCheckBox, toDoTask, deleteButton);
+  insertedDiv.setAttribute('index_id', element.index);
+  insertedDiv.append(insertedCheckBox, toDoTask, deleteButton);
 
-  TaskList.appendChild(InsertedDiv);
+  taskList.appendChild(insertedDiv);
 };
 
-const addTask = (Task) => {
-  Task.forEach((element) => {
-    createElement(element);
+// Add tasks to the DOM
+const addTask = (tasks) => {
+  tasks.forEach((task) => {
+    createElement(task);
   });
 };
 
+// Remove a task from the DOM
 const deleteTask = (item) => {
   item.parentElement.remove();
 };
-const setLocalStorage = (Task) => {
-  window.localStorage.setItem('Task', JSON.stringify(Task));
+
+// Save tasks to localStorage
+const setLocalStorage = (tasks) => {
+  window.localStorage.setItem('Task', JSON.stringify(tasks));
 };
 
+// Retrieve tasks from localStorage and add them to the DOM
 const getLocalStorage = () => {
-  const theTask = window.localStorage.getItem('Task');
+  const storedTasks = window.localStorage.getItem('Task');
 
-  if (theTask !== null) {
-    addTask(JSON.parse(theTask));
+  if (storedTasks !== null) {
+    const tasks = JSON.parse(storedTasks);
+    addTask(tasks);
+    return tasks;
   }
 
-  return theTask;
+  return [];
 };
 
+// Add a new task
 const addToDoTask = (value) => {
   const task = {
     description: value,
@@ -60,25 +72,27 @@ const addToDoTask = (value) => {
   createElement(task);
 };
 
+// Delete a task
 const deleteToDoTask = (ID) => {
-  const theTask = JSON.parse(localStorage.getItem('Task'));
-  theTask.splice(ID - 1, 1);
-  theTask.forEach((element, index) => {
-    element.index = index + 1;
+  const tasks = JSON.parse(localStorage.getItem('Task'));
+  tasks.splice(ID - 1, 1);
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
   });
-  setLocalStorage(theTask);
+  setLocalStorage(tasks);
 
-  return theTask;
+  return tasks;
 };
 
+// Edit a task
 const editToDoTask = (ID, value) => {
-  const theTask = JSON.parse(localStorage.getItem('Task'));
-  theTask.forEach((element) => {
-    if (element.index === ID) {
-      element.description = value;
+  const tasks = JSON.parse(localStorage.getItem('Task'));
+  tasks.forEach((task) => {
+    if (task.index === ID) {
+      task.description = value;
     }
-    setLocalStorage(theTask);
   });
+  setLocalStorage(tasks);
 };
 
 export {
