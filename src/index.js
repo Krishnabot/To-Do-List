50d434362a20b72c332056aeeb67f3d1f6c7a2d5
Krishna <libraryismyparadise@gmail.com>
2023-05-20 23:07:07 +0545

    refactor Index .js

--- src/index.js
+++ src/index.js
@@ -7,67 +7,82 @@ import {
   editToDoTask,
   deleteTask,
 } from './addremove.js';
-
 import { editCheckBox, clearCompletedTask } from './interactive_list.js';
 
+// Retrieve tasks from localStorage and populate the initial task list
 getLocalStorage();
 
+// Get DOM elements
 const inputTask = document.querySelector('.input-task');
 const toDoList = document.querySelector('.to-do-list');
+const addTaskButton = document.querySelector('.add-task');
+const clearButton = document.querySelector('.clr-btn');
 
-document.querySelector('.add-task').addEventListener('click', () => {
+// Event listeners
+
+// Add task button click event
+addTaskButton.addEventListener('click', () => {
   if (inputTask.value !== '') {
     addToDoTask(inputTask.value);
     inputTask.value = '';
   }
+  // Reload the page after adding a task
   window.location.reload();
 });
 
+// Input field keydown event
 inputTask.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
-    document.querySelector('.add-task').click();
+    addTaskButton.click();
   }
 });
 
+// Task list click event (delete task)
 toDoList.addEventListener('click', (event) => {
   if (event.target.classList.contains('remove-task')) {
     const item = event.target;
     deleteTask(item);
-    const iD = parseInt(
+    const id = parseInt(
       event.target.parentElement.getAttribute('index_id'),
       10,
     );
-    deleteToDoTask(iD);
+    deleteToDoTask(id);
   }
 });
 
-toDoList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('task-layout')) {
-    const iD = parseInt(e.target.parentElement.getAttribute('index_id'), 10);
-    const { value } = e.target;
-    editToDoTask(iD, value);
+// Task list click event (edit task on click)
+toDoList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('task-layout')) {
+    const id = parseInt(event.target.parentElement.getAttribute('index_id'), 10);
+    const { value } = event.target;
+    editToDoTask(id, value);
   }
 });
 
-toDoList.addEventListener('keydown', (e) => {
-  if (e.target.classList.contains('task-layout')) {
-    const iD = parseInt(e.target.parentElement.getAttribute('index_id'), 10);
-    const { value } = e.target;
-    editToDoTask(iD, value);
+// Task list keydown event (edit task on enter key press)
+toDoList.addEventListener('keydown', (event) => {
+  if (event.target.classList.contains('task-layout')) {
+    if (event.key === 'Enter') {
+      const id = parseInt(event.target.parentElement.getAttribute('index_id'), 10);
+      const { value } = event.target;
+      editToDoTask(id, value);
+    }
   }
 });
 
+// Task list click event (edit task checkbox)
 toDoList.addEventListener('click', (event) => {
   if (event.target.classList.contains('insertedcheckbox')) {
-    const insertedcheckbox = event.target;
-    const ID = parseInt(
+    const insertedCheckbox = event.target;
+    const id = parseInt(
       event.target.parentElement.getAttribute('index_id'),
       10,
     );
-    editCheckBox(ID, insertedcheckbox.checked);
+    editCheckBox(id, insertedCheckbox.checked);
   }
 });
 
-document.querySelector('.clr-btn').addEventListener('click', () => {
+// Clear completed tasks button click event
+clearButton.addEventListener('click', () => {
   clearCompletedTask();
 });
